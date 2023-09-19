371e4ad0a3fb337206867bb12d52ebc90ea5fcf8
brogan89 <brogan.king@outlook.com>
2023-09-19 15:15:25 +1200

    Added wrappers for Overlay, RemotePlay, RichPresence, Achievements

--- Packages/Steamworks.Mainframe/Runtime/Steam.cs
+++ Packages/Steamworks.Mainframe/Runtime/Steam.cs
@@ -1,4 +1,8 @@
-﻿namespace Steamworks.Mainframe
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Steamworks.Mainframe
 {
 	/// <summary>
 	/// Quick property and methods to access some steam API
@@ -9,20 +13,50 @@
 		/// Returns if SteamManager is initialised
 		/// </summary>
 		public static bool Valid => SteamManager.Initialized;
-		
+
 		/// <summary>
 		/// Id of the owner
 		/// </summary>
 		public static ulong SteamId => Valid ? SteamUser.GetSteamID().m_SteamID : 0;
-		
+
 		/// <summary>
 		/// Username of the owner
 		/// </summary>
 		public static string Username => Valid ? SteamFriends.GetPersonaName() : string.Empty;
-		
+
 		/// <summary>
 		/// The branch that steam was launched on 
 		/// </summary>
 		public static string Branch => SteamApps.GetCurrentBetaName(out var branchName, 32) ? branchName : string.Empty;
+		
+		
+		public static string GetLaunchCommandLine()
+		{
+			if (!Valid)
+				return string.Empty;
+
+			var ret = SteamApps.GetLaunchCommandLine(out string cmdLine, 512);
+
+			if (ret == -1)
+				throw new Exception("GetLaunchCommandLine failed");
+
+			Debug.Log($"GetLaunchCommandLine: '{cmdLine}'");
+
+			return cmdLine?.Trim() ?? string.Empty;
+		}
+		
+		public static IEnumerable<SteamFriend> GetFriends()
+		{
+			const EFriendFlags FLAGS = EFriendFlags.k_EFriendFlagAll;
+			var count = SteamFriends.GetFriendCount(FLAGS);
+			
+			for (int i = 0; i < count; i++)
+			{
+				var friendId = SteamFriends.GetFriendByIndex(i, FLAGS);
+				var username = SteamFriends.GetFriendPersonaName(friendId);
+				var player = new SteamFriend(friendId.m_SteamID, username);
+				yield return player;
+			}
+		}
 	}
 }
---
+++ Packages/Steamworks.Mainframe/Runtime/SteamFriend.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Steamworks.Mainframe
+{
+	[Serializable]
+	public struct SteamFriend : IEquatable<SteamFriend>, IComparable<SteamFriend>
+	{
+		public ulong SteamId;
+		public string Username;
+		
+		public bool IsMe => SteamId == Steam.SteamId;
+
+		public SteamFriend(ulong steamId, string username)
+		{
+			SteamId = steamId;
+			Username = username;
+		}
+
+		public override string ToString()
+		{
+			return $"Username: {Username}, SteamId: {SteamId}";
+		}
+
+		public bool Equals(SteamFriend other)
+		{
+			return SteamId == other.SteamId;
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is SteamFriend other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			return SteamId.GetHashCode();
+		}
+
+		public int CompareTo(SteamFriend other)
+		{
+			return SteamId.CompareTo(other.SteamId);
+		}
+	}
+}
