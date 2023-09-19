24490b9ebe8dcd08e7277d3cb28d7d3d712c4b3a
brogan89 <brogan.king@outlook.com>
2023-09-19 19:27:42 +1200

    Added steam lobbies

--- Packages/Steamworks.Mainframe/Runtime/SteamLobby.cs
+++ Packages/Steamworks.Mainframe/Runtime/SteamLobby.cs
@@ -2,64 +2,154 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Steamworks.Mainframe.Core;
 using UnityEngine;
 using UnityEngine.Scripting;
 
-namespace Steamworks.Mainframe.Core
+namespace Steamworks.Mainframe
 {
-	/// <summary>
-	/// Wrapper for lobby
-	/// </summary>
 	public static class SteamLobby
 	{
-		public static event Action<string> OnJoinRequested;
-		
-		private static TaskCompletionSource<HashSet<ulong>> _lobbyCodesTask;
-		[Preserve] private static Callback<GameLobbyJoinRequested_t> _gameLobbyJoinRequested;
+		public static event Action<SteamLobbyInfo> OnLobbyUpdated;
 
-		[RuntimeInitializeOnLoadMethod]
+		/// <summary>
+		/// Filter for searching for lobbies.
+		/// </summary>
+		public const int MaxLobbies = 60;
+		public static SteamLobbyInfo Current { get; private set; }
+
+		private static TaskCompletionSource<ulong> _createLobbyTask;
+		private static TaskCompletionSource<ulong> _joinLobbyTask;
+		private static TaskCompletionSource<HashSet<ulong>> _lobbyIdsTask;
+
+		[Preserve] private static Callback<LobbyCreated_t> _lobbyCreated;
+		[Preserve] private static Callback<GameLobbyJoinRequested_t> _lobbyRequested;
+		[Preserve] private static Callback<LobbyEnter_t> _lobbyEntered;
+		[Preserve] private static Callback<LobbyMatchList_t> _lobbyListRequest;
+		[Preserve] private static Callback<LobbyDataUpdate_t> _lobbyDataUpdated;
+
 		public static void Init()
 		{
-			_gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested_t);
+			// create
+			_lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreatedCallback);
+
+			// join
+			_lobbyRequested = Callback<GameLobbyJoinRequested_t>.Create(OnLobbyRequest);
+			_lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+
+			// get
+			_lobbyListRequest = Callback<LobbyMatchList_t>.Create(OnLobbyMatchList);
+			_lobbyDataUpdated = Callback<LobbyDataUpdate_t>.Create(OnLobbyDataUpdated);
 		}
-		
-		private static void OnGameLobbyJoinRequested_t(GameLobbyJoinRequested_t param)
+
+		#region Create
+
+		public static async Task<SteamLobbyInfo> CreateLobbyAsync(int maxConnections, bool friendsOnly = false)
 		{
-			var connect = SteamRichPresence.GetFriendConnect((ulong)param.m_steamIDFriend);
-			OnJoinRequested?.Invoke(connect);
-		}
-		
-		/// <summary>
-		/// Returns Lobby Code from friends rich presence
-		/// </summary>
-		/// <param name="friendsOnly"></param>
-		/// <param name="maxLobbies"></param>
-		/// <returns></returns>
-		public static async Task<List<string>> GetLobbyListAsync(bool friendsOnly, int maxLobbies = 60)
-		{
-			HashSet<ulong> steamPlayerIds;
+			if (!Steam.Valid)
+				throw new Exception("Steam not initialised");
+
+			var lobbyType = friendsOnly
+				? ELobbyType.k_ELobbyTypeFriendsOnly
+				: ELobbyType.k_ELobbyTypePublic;
+
+			_createLobbyTask = new TaskCompletionSource<ulong>();
+			SteamMatchmaking.CreateLobby(lobbyType, maxConnections);
+			var lobbyId = await _createLobbyTask.Task;
 			
+			Current = new SteamLobbyInfo(lobbyId)
+			{
+				HostId = Steam.SteamId,
+				LobbyName = $"{Steam.Username}'s Game",
+				AppVersion = Application.version,
+				IsActive = true,
+				MaxConnections = maxConnections,
+			};
+
+			UpdateLobbyPlayerCount(1);
+			SteamRichPresence.SetConnect(Steam.SteamId.ToString());
+
+			return Current;
+		}
+
+		private static void OnLobbyCreatedCallback(LobbyCreated_t callback)
+		{
+			if (callback.m_eResult != EResult.k_EResultOK)
+			{
+				Debug.LogError("Failed to created lobby");
+				return;
+			}
+
+			Debug.Log($"Lobby created: {callback.m_ulSteamIDLobby}");
+			_createLobbyTask.SetResult(callback.m_ulSteamIDLobby);
+		}
+
+		#endregion
+
+		#region Join
+
+		public static async Task<SteamLobbyInfo> JoinLobbyAsync(ulong lobbyId)
+		{
+			if (!Steam.Valid)
+				throw new Exception("Steam not initialised");
+
+			_joinLobbyTask = new TaskCompletionSource<ulong>();
+			SteamMatchmaking.JoinLobby((CSteamID)lobbyId);
+			Current = new SteamLobbyInfo(lobbyId);
+			await _joinLobbyTask.Task;
+			return Current;
+		}
+
+		private static void OnLobbyRequest(GameLobbyJoinRequested_t callback)
+		{
+			Debug.Log($"Lobby request from {callback.m_steamIDFriend}");
+			SteamMatchmaking.JoinLobby((CSteamID)Current.LobbyId);
+		}
+
+		private static void OnLobbyEntered(LobbyEnter_t callback)
+		{
+			Debug.Log($"Lobby Entered: {callback.m_ulSteamIDLobby}");
+			_joinLobbyTask.SetResult(0);
+		}
+
+		#endregion
+
+		#region Leave
+
+		public static void LeaveLobby()
+		{
+			if (Steam.Valid)
+				SteamMatchmaking.LeaveLobby((CSteamID)Current.LobbyId);
+		}
+
+		#endregion
+
+		#region Lobby List
+
+		public static async Task<List<SteamLobbyInfo>> GetLobbyListAsync(bool friendsOnly)
+		{
+			HashSet<ulong> lobbyIds;
+
 			if (friendsOnly)
 			{
-				steamPlayerIds = new HashSet<ulong>();
+				lobbyIds = new HashSet<ulong>();
 				foreach (var lobbyId in GetLobbyIdsFromFriends())
-					steamPlayerIds.Add(lobbyId);
+					lobbyIds.Add(lobbyId);
 			}
 			else
 			{
-				_lobbyCodesTask = new TaskCompletionSource<HashSet<ulong>>();
-				SteamMatchmaking.AddRequestLobbyListResultCountFilter(maxLobbies);
+				_lobbyIdsTask = new TaskCompletionSource<HashSet<ulong>>();
+				SteamMatchmaking.AddRequestLobbyListResultCountFilter(MaxLobbies);
 				SteamMatchmaking.RequestLobbyList();
-				steamPlayerIds = await _lobbyCodesTask.Task;
+				lobbyIds = await _lobbyIdsTask.Task;
 			}
 
-			// return lobby codes
-			return steamPlayerIds
-				.Select(SteamRichPresence.GetFriendConnect)
-				// .Where(info => info.IsActive && info.AppVersion == App.VersionString)
+			return lobbyIds
+				.Select(x => new SteamLobbyInfo(x))
+				.Where(info => info.IsActive && info.AppVersion == Application.version)
 				.ToList();
 		}
-		
+
 		/// <summary>
 		/// Docs: https://partner.steamgames.com/doc/features/multiplayer/matchmaking
 		/// </summary>
@@ -72,9 +162,45 @@ namespace Steamworks.Mainframe.Core
 				var isInGame = SteamFriends.GetFriendGamePlayed(steamIDFriend, out var friendGameInfo);
 				var isThisGame = friendGameInfo.m_gameID.m_GameID == Steam.AppId;
 				if (isInGame && isThisGame && friendGameInfo.m_steamIDLobby.IsValid())
+				{
+					SteamMatchmaking.RequestLobbyData(friendGameInfo.m_steamIDLobby);
 					yield return friendGameInfo.m_steamIDLobby.m_SteamID;
+				}
 			}
 		}
-		
+
+		private static void OnLobbyMatchList(LobbyMatchList_t callback)
+		{
+			if (_lobbyIdsTask == null)
+				throw new NullReferenceException($"{nameof(_lobbyIdsTask)} is null");
+
+			var lobbyIds = new HashSet<ulong>();
+			for (int i = 0; i < callback.m_nLobbiesMatching; i++)
+			{
+				var lobbyId = SteamMatchmaking.GetLobbyByIndex(i);
+				lobbyIds.Add((ulong)lobbyId);
+				SteamMatchmaking.RequestLobbyData(lobbyId);
+			}
+
+			_lobbyIdsTask.SetResult(lobbyIds);
+		}
+
+		#endregion
+
+		#region Updates
+
+		private static void OnLobbyDataUpdated(LobbyDataUpdate_t param)
+		{
+			var info = new SteamLobbyInfo(param.m_ulSteamIDLobby);
+			OnLobbyUpdated?.Invoke(info);
+		}
+
+		private static void UpdateLobbyPlayerCount(int playerCount)
+		{
+			Current.PlayerCount = playerCount;
+			SteamRichPresence.SetGroup(playerCount);
+		}
+
+		#endregion
 	}
 }
---
+++ Packages/Steamworks.Mainframe/Runtime/SteamLobbyInfo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace Steamworks.Mainframe.Core
+{
+	public class SteamLobbyInfo
+	{
+		public ulong LobbyId { get; }
+
+		public ulong HostId
+		{
+			get => ParseUlong(nameof(HostId));
+			set => SetData(nameof(HostId), value.ToString());
+		}
+
+		public string LobbyName
+		{
+			get => GetData(nameof(LobbyName));
+			set => SetData(nameof(LobbyName), value);
+		}
+
+		public int PlayerCount
+		{
+			get => ParseInt(nameof(PlayerCount));
+			set => SetData(nameof(PlayerCount), value.ToString());
+		}
+
+		public string AppVersion
+		{
+			get => GetData(nameof(AppVersion));
+			set => SetData(nameof(AppVersion), value);
+		}
+
+		public bool IsActive
+		{
+			get => GetData(nameof(IsActive)) == true.ToString();
+			set => SetData(nameof(IsActive), value.ToString());
+		}
+
+		public int MaxConnections 
+		{
+			get => ParseInt(nameof(MaxConnections));
+			set => SetData(nameof(MaxConnections), value.ToString());
+		}
+
+		public SteamLobbyInfo(ulong lobbyId)
+		{
+			LobbyId = lobbyId;
+		}
+
+		private int ParseInt(string key)
+		{
+			var rawVal = GetData(key);
+
+			if (int.TryParse(rawVal, out var id))
+				return id;
+
+			Debug.LogError($"Could not parse '{rawVal}' to {nameof(UInt32)}. Key: {key}");
+			return 0;
+		}
+		
+		private ulong ParseUlong(string key)
+		{
+			var rawVal = GetData(key);
+
+			if (ulong.TryParse(rawVal, out var id))
+				return id;
+
+			Debug.LogError($"Could not parse '{rawVal}' to {nameof(UInt64)}. Key: {key}");
+			return 0;
+		}
+
+		public override string ToString()
+		{
+			return JObject.FromObject(this).ToString();
+		}
+
+		private string GetData(string key)
+		{
+			return SteamMatchmaking.GetLobbyData((CSteamID)LobbyId, key);
+		}
+
+		private void SetData(string key, string value)
+		{
+			if (!SteamMatchmaking.SetLobbyData((CSteamID)LobbyId, key, value))
+				Debug.LogError($"Failed to set lobby data. {key}: {value}");
+		}
+	}
+}
